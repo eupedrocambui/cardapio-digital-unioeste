5be350c673842395fca98a21e3eff9227a6d5622
eupedrocambui <pedrocambui06@gmail.com>
2025-12-23 11:51:48 -0300

    Header responsivo com menu hamburguer

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,18 @@
 fetch("/header.html")
   .then(response => response.text())
   .then(data => {
-    document.getElementById("header").innerHTML = data;
+    // Carregando o header no DOM
+    document.getElementById("header").innerHTML = data; 
 
+    // Tornando o menu hamburguer clicável
+    const hamburguerIcon = document.querySelector('.hamburguer');
+    const menu = document.querySelector('.menu');
+
+    hamburguerIcon.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+
+    // Aplicando nav-selected no link correto a partir da URL
     const currentPage = window.location.pathname.split("/").pop() || "index.html";
 
     document.querySelectorAll(".nav-link").forEach(link => {
